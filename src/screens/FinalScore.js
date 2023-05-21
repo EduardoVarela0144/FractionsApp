6859cc679318ab2bd21011929f8e24733e8791aa
Eduardo Varela <014426634@ulsaoaxaca.edu.mx>
2023-05-21 15:47:01 -0600

    Scroll view

--- src/screens/FinalScore.js
+++ src/screens/FinalScore.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Text, View, Image, Button, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import { styles } from "../assets/styles";
 import Men from "../assets/images/Men.png";
 import Punk from "../assets/images/Punk.png";
@@ -36,49 +43,56 @@ export default function FinalScore(props) {
   };
 
   return (
-    <View style={{ alignItems: "center" }}>
-      <View style={styles.bg} />
-      <View
-        style={{
-          alignItems: "center",
-          justifyContent: "center",
-          position: "relative",
-        }}
-      >
-        <Image
-          source={selectedImage}
-          style={{ width: "60%", aspectRatio: 1, height: "60%" }}
-          resizeMode="contain"
-        />
-        <Text
+    <ScrollView>
+      <View style={{ alignItems: "center" }}>
+        <View style={styles.bg} />
+        <View
           style={{
-            fontSize: 25,
-            color: "white",
-            fontWeight: "bold",
-            marginVertical: "3%",
+            alignItems: "center",
+            justifyContent: "center",
+            position: "relative",
           }}
         >
-          {params.name}{" "}
-        </Text>
-      </View>
-      <View style={{ alignItems: "center" }}>
-        <Text style={{ fontSize: 25 }}> Your Score </Text>
-        <Text style={score}> {params.score} / 100 </Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: "#ADE1FF",
-          width: "40%",
-          borderRadius: 10,
-          marginTop: "5%",
-        }}
-      >
-        <TouchableOpacity onPress={goToHome} style={{ paddingVertical: "3%" }}>
-          <Text style={{ fontSize: 30, color: "#109DFF", textAlign: "center" }}>
-            Finish
+          <Image
+            source={selectedImage}
+            style={{ width: "60%", aspectRatio: 1, height: "60%" }}
+            resizeMode="contain"
+          />
+          <Text
+            style={{
+              fontSize: 25,
+              color: "white",
+              fontWeight: "bold",
+              marginVertical: "3%",
+            }}
+          >
+            {params.name}{" "}
           </Text>
-        </TouchableOpacity>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ fontSize: 25 }}> Your Score </Text>
+          <Text style={score}> {params.score} / 100 </Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "#ADE1FF",
+            width: "40%",
+            borderRadius: 10,
+            marginTop: "5%",
+          }}
+        >
+          <TouchableOpacity
+            onPress={goToHome}
+            style={{ paddingVertical: "3%" }}
+          >
+            <Text
+              style={{ fontSize: 30, color: "#109DFF", textAlign: "center" }}
+            >
+              Finish
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
